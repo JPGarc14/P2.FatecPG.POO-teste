0c0d2d4e4c382be4144285433b8a1af92e9bce41
JPGarc14 <jpgarciaslv@gmail.com>
2020-11-30 03:29:48 -0300

    Disciplinas

--- P2.FatecPG.POO-teste/src/java/p1/poo/fatecpg/ads/noite/Disciplinas.java
+++ P2.FatecPG.POO-teste/src/java/p1/poo/fatecpg/ads/noite/Disciplinas.java
@@ -1,31 +1,111 @@
 package p1.poo.fatecpg.ads.noite;
 import java.util.ArrayList;
+import java.sql.*;
+import web.DBListener;
+
 public class Disciplinas {
-    ArrayList<Disciplinas> disciplinas = new ArrayList<>();
-    String nome;
-    String ementa;
-    int ciclo;
-    float nota;
+    private String nome;
+    private String ementa;
+    private int ciclo;
+    private double nota;
 
-    public Disciplinas(String nome, String ementa, int ciclo) {
+    public Disciplinas(String nome, String ementa, int ciclo, double nota) {
         this.nome = nome;
         this.ementa = ementa;
         this.ciclo = ciclo;
+        this.nota = nota;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public static ArrayList<Disciplinas> getList() throws Exception{
+        ArrayList<Disciplinas> list = new ArrayList<>();
+        Connection con = null; Statement stmt = null; ResultSet rs = null;
+        Exception methodException = null;
+        try {
+            con = DBListener.getConnection();
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM disciplinas;");
+            while(rs.next()){
+            list.add(new Disciplinas(
+            rs.getString("nome"),
+            rs.getString("ementa"),        
+            rs.getInt("ciclo"),        
+            rs.getDouble("nota")
+            ));
+            }
+            
+        } catch (Exception ex) {
+        }finally {
+            try{stmt.close();}catch(Exception ex2){}
+            try{con.close();}catch(Exception ex2){}
+            try{rs.close();}catch(Exception ex2){} 
+        }
+        if ( methodException != null)throw methodException;
+        return list;
+    }
+    public static void Insert(String nome, String ementa, int ciclo, double nota) throws Exception{
+        Connection con = null; PreparedStatement stmt = null; ResultSet rs = null;
+        Exception methodException = null;
+        try {
+            con = DBListener.getConnection();
+            stmt = con.prepareStatement("INSERT INTO disciplinas VALUES(?, ?, ?, ?);");
+            stmt.setString(1, nome);
+            stmt.setString(2, ementa);
+            stmt.setInt(3, ciclo);
+            stmt.setDouble(4, nota);
+            stmt.execute();
+            
+        } catch (Exception ex) {
+        }finally {
+            try{stmt.close();}catch(Exception ex2){}
+            try{con.close();}catch(Exception ex2){}
+            try{rs.close();}catch(Exception ex2){} 
+        }
+        if ( methodException != null)throw methodException;
+    }
+    public static void Update(String nomeOld, String nome, String ementa, int ciclo, double nota) throws Exception{
+        Connection con = null; PreparedStatement stmt = null; ResultSet rs = null;
+        Exception methodException = null;
+        try {
+            con = DBListener.getConnection();
+            stmt = con.prepareStatement("UPDATE disciplinas SET nome=?, ementa=?, ciclo=?, nota=? WHERE nome=?;");
+            stmt.setString(1, nome);
+            stmt.setString(2, ementa);
+            stmt.setInt(3, ciclo);
+            stmt.setDouble(4, nota);
+            stmt.setString(5, nomeOld);
+            stmt.execute();
+            
+        } catch (Exception ex) {
+        }finally {
+            try{stmt.close();}catch(Exception ex2){}
+            try{con.close();}catch(Exception ex2){}
+            try{rs.close();}catch(Exception ex2){} 
+        }
+        if ( methodException != null)throw methodException;
+    }
+    public static void Delete(String nome) throws Exception{
+        Connection con = null; PreparedStatement stmt = null; ResultSet rs = null;
+        Exception methodException = null;
+        try {
+            con = DBListener.getConnection();
+            stmt = con.prepareStatement("DELETE FROM disciplinas  WHERE nome=?;");
+            stmt.setString(1, nome);
+            stmt.execute();
+            
+        } catch (Exception ex) {
+        }finally {
+            try{stmt.close();}catch(Exception ex2){}
+            try{con.close();}catch(Exception ex2){}
+            try{rs.close();}catch(Exception ex2){} 
+        }
+        if ( methodException != null)throw methodException;
+    }
+    
+    public double getNota() {
+        return nota;
     }
 
-    public void setEmenta(String ementa) {
-        this.ementa = ementa;
-    }
-
-    public void setCiclo(int ciclo) {
-        this.ciclo = ciclo;
-    }
-
-    public void setNota(float nota) {
+    public void setNota(double nota) {
         this.nota = nota;
     }
 
@@ -33,26 +113,34 @@ public class Disciplinas {
         return nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public String getEmenta() {
         return ementa;
+    }
+
+    public void setEmenta(String ementa) {
+        this.ementa = ementa;
     }
 
     public int getCiclo() {
         return ciclo;
     }
 
-    public float getNota() {
-        return nota;
+    public void setCiclo(int ciclo) {
+        this.ciclo = ciclo;
     }
     
-    public void setList (ArrayList<Disciplinas> list ) {
-        this.disciplinas = list;
+    public static String getCreateStatement(){
+        return "CREATE TABLE IF NOT EXISTS disciplinas("
+                + "nome VARCHAR (200) PRIMARY KEY,"
+                + "ementa VARCHAR (200) NOT NULL,"
+                + "ciclo NUMBER (2) NOT NULL,"
+                + "nota NUMBER (2,2) NOT NULL"
+                + ");";
+       
     }
     
-    public void adicionarLista (Disciplinas disci) {
-        disciplinas.add(disci);
-    }
-    public ArrayList<Disciplinas> getList() {
-        return disciplinas;
-    }
 }
--- P2.FatecPG.POO-teste/src/java/web/DBListener.java
+++ P2.FatecPG.POO-teste/src/java/web/DBListener.java
@@ -5,10 +5,37 @@
  */
 package web;
 
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.sql.*;
 /**
  * Web application lifecycle listener.
  *
  * @author JPGARCIA
  */
-public class DBListener {
+public class DBListener implements ServletContextListener {
+public static final String CLASS_NAME = "org.sqlite.JDBC";
+public static final String DB_URL = "jdbc:sqlite:C:\\Users\\jpgar\\OneDrive\\Área de Trabalho\\tasks-noturno.db";
+public static String exceptionMessage = null;
+public static Connection getConnection()throws Exception{
+    return DriverManager.getConnection(DB_URL);
+} 
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+    Connection con = null; Statement stmt = null;
+        try {
+            Class.forName(CLASS_NAME);
+            con = DriverManager.getConnection(DB_URL);
+            stmt = con.createStatement();
+        } catch (Exception ex) {
+        }finally {
+            try{stmt.close();}catch(Exception ex2){}
+            try{con.close();}catch(Exception ex2){}   
+            }
+        }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
 }
